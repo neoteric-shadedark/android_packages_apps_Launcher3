76994987f92d07030fb31473eb967101487e11ed
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-17 16:54:56 +0300

    Launcher3: Add an option to open the keyboard with the drawer

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -159,7 +159,6 @@ import android.view.WindowInsetsAnimation;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.OvershootInterpolator;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 import android.window.BackEvent;
 import android.window.OnBackAnimationCallback;
@@ -456,7 +455,7 @@ public class Launcher extends StatefulActivity<LauncherState>
                 }
             };
 
-    private InputMethodManager mInputMethodManager;
+    private boolean mWasImeOpened = false;
 
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
@@ -651,9 +650,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         TestEventEmitter.sendEvent(TestEvent.LAUNCHER_ON_CREATE);
 
         mSharedPrefs.registerOnSharedPreferenceChangeListener(mSharedPrefListener);
-
-        mInputMethodManager = (InputMethodManager) mWorkspace.getContext().getSystemService(
-                Context.INPUT_METHOD_SERVICE);
     }
 
     protected ModelCallbacks createModelCallbacks() {
@@ -2885,11 +2881,18 @@ public class Launcher extends StatefulActivity<LauncherState>
      * @param progress Transition progress from 0 to 1; where 0 => home and 1 => all apps.
      */
     public void onAllAppsTransition(float progress) {
-        if (progress > 0) return;
-        if (mAppsView == null) return;
-        if (mInputMethodManager == null) return;
-        // make sure the keyboard is hidden when the AllApps view is closed
-        mInputMethodManager.hideSoftInputFromWindow(mAppsView.getWindowToken(), 0);
+        if (progress == 1f && !mWasImeOpened &&
+                Utilities.enableAutoIme(mWorkspace.getContext())) {
+            ExtendedEditText editText = mAppsView.getSearchUiManager().getEditText();
+            if (editText == null) return;
+            editText.showKeyboard();
+            mWasImeOpened = true;
+            return;
+        }
+        if (progress == 0 && mAppsView != null) {
+            hideKeyboard();
+        }
+        mWasImeOpened = false;
     }
 
     /**
--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -170,6 +170,7 @@ public final class Utilities {
     public static final String KEY_SINGLE_PAGE_CENTER = "pref_single_page_center";
     public static final String KEY_DRAWER_SCROLLBAR = "pref_drawer_scrollbar";
     public static final String KEY_SMARTSPACE = "pref_smartspace";
+    public static final String KEY_AUTO_KEYABORD = "pref_auto_keyboard";
 
     /**
      * Returns true if theme is dark.
@@ -1062,5 +1063,10 @@ public final class Utilities {
     private static boolean isSmartspaceEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_SMARTSPACE, true);
+    }
+
+    public static boolean enableAutoIme(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_AUTO_KEYABORD, false);
     }
 }
